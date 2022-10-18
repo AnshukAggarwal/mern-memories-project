ea4f9833dddaa380ffb097dba3e07a5a66c574e7
Anshuk Aggarwal <aggar@LAPTOP-OSKI3RTS>
2022-10-18 12:54:53 -0400

    made updates to deploy app to heroku

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,10 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 const mongoose = require("mongoose");
+const path = require("path");
 const memoriesRouter = require("./Routes/memories.routes");
 
-const port = process.env.PORT || 500;
+const port = process.env.PORT || 5000;
 
 const MONGODB_URL = process.env.DBURL;
 
@@ -16,6 +17,18 @@ app.use(express.urlencoded({ extended: true })); //added to data sent via the po
 app.use(cors());
 
 app.use("/memories", memoriesRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.status(200).send("Welcome");
+  });
+}
 
 mongoose.connect(MONGODB_URL, { useNewUrlParser: true });
 
